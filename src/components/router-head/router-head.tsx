0042879fe49d4e49fe3aaea7248d1a1679b15259
Grandschtroumpf <francois.guezengar@hotmail.fr>
2025-05-13 10:03:02 +0200

    add google site verification

--- src/components/router-head/router-head.tsx
+++ src/components/router-head/router-head.tsx
@@ -16,6 +16,7 @@ export const RouterHead = component$(() => {
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="theme-color" content="#f7f8ff" media="(prefers-color-scheme: light)" />
       <meta name="theme-color" content="#05050d" media="(prefers-color-scheme: dark)" />
+      <meta name="google-site-verification" content="rCZ_OI2PmTs3Un_IOjuBXFCBZgL7x2EDM4vPPEwz-4s" />
 
       {head.meta.map((m) => (
         <meta key={m.key} {...m} />
