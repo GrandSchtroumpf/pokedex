50a0b5a4def74768773fa1d4625334cf84bfbd8a
Grandschtroumpf <francois.guezengar@hotmail.fr>
2025-05-23 12:56:36 +0200

    remove debug

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(() => {
   return {
-    plugins: [qwikCity(), qwikVite({ debug: true }), tsconfigPaths()],
+    plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
     preview: {
       headers: {
         "Cache-Control": "public, max-age=600",
